74f4ced9e96a9a9792cf20cebafb126616293d34
Unique Orduna <uniqueorduna@swuser-129-8-205-23.swlan.csufresno.edu>
2022-03-24 14:58:34 -0700

    changes

--- test_1.py
+++ test_1.py
@@ -15,3 +15,35 @@ if Option 1 pays more, output to the user "Option 1 is better"
 if Option 2 pays more, output to the user "Option 2 is better"
 
 """
+
+
+"""
+#Option 1
+    return 100 * 10
+
+
+#Option 2
+    amount = 1
+    list1 = []
+    loop 10 times
+        add amount to list1
+        amount *=2
+    sum = sum all items in loop
+    return sum
+
+
+#Main
+    var1 = Option 1
+    var2 = Option 2
+
+if var1 = var2
+    "Option 1 and Option 2 pays the same"
+if var1 < var2
+    "Option 2 is better"
+
+else 
+    "Option 1 is better"
+
+
+Main
+"""
